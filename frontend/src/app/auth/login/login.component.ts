5ea4bcdce73b0da3b58ebaa5c31e2511bf211c52
golu7679 <golurajak811@gmail.com>
2023-03-06 21:32:42 +0530

    Update: login page design updated

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -3,6 +3,8 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ALL_TAIGA_UI_MODULES } from 'src/app/all-taiga.modules';
+import { validationConfig } from 'src/app/helper/custom-validator';
+import { AlertService } from 'src/app/services/alert/alert.service';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +12,13 @@ import { ALL_TAIGA_UI_MODULES } from 'src/app/all-taiga.modules';
   styleUrls: ['./login.component.less'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, ReactiveFormsModule, RouterModule, ALL_TAIGA_UI_MODULES]
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, ALL_TAIGA_UI_MODULES],
+  providers: [validationConfig],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+
+  constructor(private formBuilder: FormBuilder, private alertService: AlertService) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]]
@@ -23,5 +27,9 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  loginFormSubmit() {
+    this.loginForm.markAllAsTouched();
   }
 }
